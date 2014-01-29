673d58d524698347191c3747bceff62ba879b022
jlgiga <juvylou.giga@g.msuiit.edu.ph>
2014-01-29 20:04:28 +0800

    add functionality for inserting data to the database

--- platforms/android/src/com/project/db/DatabaseHelper.java
+++ platforms/android/src/com/project/db/DatabaseHelper.java
@@ -1,5 +1,8 @@
 package com.project.db;
 
+import com.project.db.DatabaseContract.ActivityTable;
+
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
@@ -9,18 +12,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	
 	private static final String CREATE_TABLE_ACTIVTY_TRACKER =
 			"CREATE TABLE " + DatabaseContract.ActivityTable.TABLE_NAME + " (" +
-					DatabaseContract.ActivityTable.COLUMN_NAME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-		    		DatabaseContract.ActivityTable.COLUMN_NAME_TITLE + " TEXT UNIQUE NOT NULL," +
-		    		DatabaseContract.ActivityTable.COLUMN_NAME_DESCRIPTION + " TEXT," +
-		    		DatabaseContract.ActivityTable.COLUMN_NAME_LOCATION + " TEXT," +
-		    		DatabaseContract.ActivityTable.COLUMN_NAME_START_DATE + " DATE," +
-		    		DatabaseContract.ActivityTable.COLUMN_NAME_END_DATE + " DATE," +
-		    		DatabaseContract.ActivityTable.COLUMN_NAME_START_TIME + " DATETIME," +
-		    		DatabaseContract.ActivityTable.COLUMN_NAME_END_TIME + " DATETIME," +
-		    		DatabaseContract.ActivityTable.COLUMN_NAME_PRIORITY + " TEXT," +
-		    		DatabaseContract.ActivityTable.COLUMN_NAME_ALERT + " TEXT NOT NULL," +
-		    		DatabaseContract.ActivityTable.COLUMN_NAME_REPETITION + " TEXT," +
-		    		DatabaseContract.ActivityTable.COLUMN_NAME_NOTIFICATION + " TEXT," +
+					ActivityTable.COLUMN_NAME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+		    		ActivityTable.COLUMN_NAME_TITLE + " TEXT UNIQUE NOT NULL," +
+		    		ActivityTable.COLUMN_NAME_DESCRIPTION + " TEXT," +
+		    		ActivityTable.COLUMN_NAME_LOCATION + " TEXT," +
+		    		ActivityTable.COLUMN_NAME_START_DATE + " DATE," +
+		    		ActivityTable.COLUMN_NAME_END_DATE + " DATE," +
+		    		ActivityTable.COLUMN_NAME_START_TIME + " DATETIME," +
+		    		ActivityTable.COLUMN_NAME_END_TIME + " DATETIME," +
+		    		ActivityTable.COLUMN_NAME_PRIORITY + " TEXT," +
+		    		ActivityTable.COLUMN_NAME_ALERT + " TEXT NOT NULL," +
+		    		ActivityTable.COLUMN_NAME_REPETITION + " TEXT," +
+		    		ActivityTable.COLUMN_NAME_NOTIFICATION + " TEXT," +
 		    " )";
 
 	private static final String DELETE_TABLE_ACTIVTY_TRACKER =
@@ -42,5 +45,31 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	/**
+	 * All CRUD(Create, Read, Update, Delete) Operations
+	 */
+
+	// Adding new activity
+	public void createActivity(ActivityModel activity) {
+		SQLiteDatabase db = this.getWritableDatabase();
+
+		ContentValues values = new ContentValues();
+		values.put(ActivityTable.COLUMN_NAME_TITLE, activity.getTitle());
+		values.put(ActivityTable.COLUMN_NAME_DESCRIPTION, activity.getDescription());
+		values.put(ActivityTable.COLUMN_NAME_LOCATION, activity.getLocation());
+		values.put(ActivityTable.COLUMN_NAME_START_DATE, activity.getStart_date());
+		values.put(ActivityTable.COLUMN_NAME_END_DATE, activity.getEnd_date());
+		values.put(ActivityTable.COLUMN_NAME_START_TIME, activity.getStart_time());
+		values.put(ActivityTable.COLUMN_NAME_END_TIME, activity.getEnd_time());
+		values.put(ActivityTable.COLUMN_NAME_PRIORITY, activity.getPriority());
+		values.put(ActivityTable.COLUMN_NAME_ALERT, activity.getAlert());
+		values.put(ActivityTable.COLUMN_NAME_REPETITION, activity.getRepetition());
+		values.put(ActivityTable.COLUMN_NAME_NOTIFICATION, activity.getNotification());
+
+		// insert row
+		db.insert(CREATE_TABLE_ACTIVTY_TRACKER, null, values);
+		db.close();
 	}
 }
