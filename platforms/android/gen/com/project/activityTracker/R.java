e1263dcca26fbc62aa24a2ed1c85e55bf871c8da
eRuiz <eruiz@ubuntu.(none)>
2014-01-30 14:25:00 +0800

    added activity model and UI for read activities

--- platforms/android/gen/com/project/activityTracker/R.java
+++ platforms/android/gen/com/project/activityTracker/R.java
@@ -13,10 +13,13 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class layout {
+        public static final int main_screen=0x7f030000;
+    }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
     }
     public static final class xml {
-        public static final int config=0x7f030000;
+        public static final int config=0x7f040000;
     }
 }
