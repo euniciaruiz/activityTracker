986ac8a7d2322654a6bc7076c5d1350f6ee1e85c
jlgiga <juvylou.giga@g.msuiit.edu.ph>
2014-02-20 16:10:43 +0800

    add create activity functionality

--- activityTracker/platforms/android/assets/www/js/model.js
+++ activityTracker/platforms/android/assets/www/js/model.js
@@ -40,3 +40,30 @@ function createTableIfNotExists() {
         }
     );
 }
+
+function insertRecord() {
+    console.debug('called insertRecord()');
+
+	var title = $('#title').val();
+    var description = $('#description').val();
+    var location = $('#location').val();
+    var start_date = $('#start_date').val();
+    var end_date = $('#end_date').val();
+    var start_time = $('#start_time').val();
+    var end_time = $('#end_time').val();
+    var priority = $('#priority').val();
+    var alert = $('#alert').val();
+    var repetition = $('#repetition').val();
+    var notification = $('#notification').val();
+
+    var sql = 'INSERT INTO Contacts (title, description, location, start_date, end_date, start_time, end_time,' +
+    								'priority, alert, repetition, notification) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+
+    db.transaction(
+        function (transaction) {
+            transaction.executeSql(sql, [title, description, location, start_date, end_date, start_time, end_time,
+    									 priority, alert, repetition, notification], showRecordsAndResetForm, handleErrors);
+            console.debug('executeSql: ' + sql);
+        }
+    );
+}
